600307921b71453608243b35e459229e7faf8d34
Mats Gundersen <matsgundersen@hotmail.com>
2023-10-13 10:41:25 +0200

    feat: bootstrap sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'wedding-page-sanity',
+
+  projectId: 'pisxls7h',
+  dataset: 'production',
+
+  plugins: [
+    deskTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
